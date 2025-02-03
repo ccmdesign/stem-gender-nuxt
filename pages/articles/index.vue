6dc1738730791b8bed2f7bb549db6ec44f602b16
Claudio Mendonca <claudioccm@gmail.com>
2025-02-02 16:30:14 -0800

    more setup

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,24 +1,53 @@
 
 <template>
   <main>
-    <ContentList :query="query" :key="'articleList' + locale">
-      <template #default="{ list }">
-        <ul>
-          <li v-for="article in list" :key="article._path">
-            <NuxtLink :to="localePath(article._path)"
-              >{{ article.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </template>
-      <template #not-found>
-        <p>No articles found.</p>
-      </template>
-      <template #pending>
-        <p>...</p>
-      </template>
-    </ContentList>
-  </main>
+    <header class="chapter-layout__header | chapter-header">
+      <div class="repel">
+        <div class="language-switcher">
+          <a href="/en/">EN</a>
+          <a href="/es/">ES</a>
+          <a href="/fr/">FR</a>
+        </div>
+
+        <h3>Synthesis Report</h3>
+      </div>
+
+      <hgroup>
+        <h1>
+          <span>Breaking Barriers</span>
+          <span>Building Bridges</span>
+        </h1>
+
+        <div class="chapter-header__extras">
+          <a href="#" download>Podcast(AI)</a>
+          <a href="#" download>Audio Version</a>
+          <a href="#" download>Download PDF</a>
+        </div>
+
+        <h2 class="chapter-header__subtitle">A Southern-led research network to advance gender equality in STEM</h2>
+      </hgroup>
+
+      <ul class="chapter-header__toc">
+        <ContentList :query="query" :key="'articleList' + locale">
+          <template #default="{ list }">
+            <ul>
+              <li v-for="article in list" :key="article._path">
+                <NuxtLink :to="localePath(article._path)"
+                  >{{ article.title }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </template>
+          <template #not-found>
+            <p>No articles found.</p>
+          </template>
+          <template #pending>
+            <p>...</p>
+          </template>
+        </ContentList>
+      </ul>
+    </header>
+  </main> 
 </template>
 
 <script setup lang="ts">
@@ -30,3 +59,45 @@ const query: QueryBuilderParams = {
 }
 console.log(query)
 </script>
+
+
+<!-- <article class="chapter-layout">
+  <header class="chapter-layout__header | chapter-header">
+    <div class="repel">
+      <div class="language-switcher">
+        <a href="/en/">EN</a>
+        <a href="/es/">ES</a>
+        <a href="/fr/">FR</a>
+      </div>
+
+      <h3>Synthesis Report</h3>
+    </div>
+
+    <hgroup>
+      <h1>
+        <span>Breaking Barriers</span>
+        <span>Building Bridges</span>
+      </h1>
+
+      <div class="chapter-header__extras">
+        <a href="#" download>Podcast(AI)</a>
+        <a href="#" download>Audio Version</a>
+        <a href="#" download>Download PDF</a>
+      </div>
+
+      <h2 class="chapter-header__subtitle">A Southern-led research network to advance gender equality in STEM</h2>
+    </hgroup>
+
+    <ul class="chapter-header__toc">
+      {% set currentLang = lang | default('en') %}
+      {% for chapter in collections.orderedReport | filterByLang(currentLang) %}
+        <li>
+          <a href="{{ chapter.url }}">{{ chapter.data.title | default("Untitled") }}</a>
+        </li>
+      {% endfor %}
+    </ul>
+  </header>
+  <main class="chapter-layout__content">
+    {% include "partials/chapter-main.njk" %}
+  </main>
+</article> -->
