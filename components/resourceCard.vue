aac61821be7b8537257d20c7807589f267ad4af1
Claudio Mendonca <claudioccm@gmail.com>
2025-02-10 20:25:38 -0800

    design tweaks on homepage and articles

--- components/resourceCard.vue
+++ components/resourceCard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="resource-card | map-grid__content | stack" :class="{ 'hidden': !isVisible }">
+  <div class="resource-card | stack" :class="{ 'hidden': !isVisible }">
     <div class="repel">
       <h5 class="resource-card__country">{{ resource.region }}</h5>
       <h5 class="resource-card__status">{{ resource.status }}</h5>
     </div>
-    <h3 class="resource-card__title">{{ resource.name }}</h3>
+    <h2 class="resource-card__title">{{ resource.name }}</h2>
     <p class="resource-card__description">{{ resource.project_type }}</p>
-    <a :href="resource.url" target="_blank">{{ resource.url }}</a>
+    <a class="button" data-color="tertiary" data-visual="primary" :href="resource.url" target="_blank">Read More</a>
   </div>
 </template>
 
@@ -27,17 +27,18 @@ defineProps({
 <style lang="scss" scoped>
 .resource-card {
   padding: var(--space-m);
-  border: 1px solid var(--base-color);
+  border: 1px solid var(--primary-color-30-tint);
   background-color: var(--white-color);
+  box-shadow: var(--box-shadow-m);
 }
 
 .resource-card__country,
 .resource-card__status {
-  font-size: var(--size--2);
+  font-size: var(--size--1);
   font-family: var(--font-display);
   font-weight: 600;
-  letter-spacing: .4px;
-  color: var(--base-color);
+  letter-spacing: .5px;
+  color: var(--primary-color);
 }
 
 .resource-card__title {
