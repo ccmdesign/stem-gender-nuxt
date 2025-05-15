09043cf8cea6f79daef3719625fd80505a135a05
Douglas Cantao <dcp.pereira@gmail.com>
2025-05-15 09:47:56 -0700

    chore: remove eslint and prettier dependencies from devDependencies adding nitro-preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   target: 'static',
+  nitro: {
+    preset: 'netlify-static'
+  },
   ssr: true,
   devtools: {
     enabled: true,
