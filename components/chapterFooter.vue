4b250d7bfde465fc3b60669f839df738da29c7a7
Douglas Cantao <dcp.pereira@gmail.com>
2025-06-03 11:01:39 -0700

    fix: add alt attribute to footer logo image for accessibility

--- components/chapterFooter.vue
+++ components/chapterFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer">
-      <img class="logo" src="/images/logo-footer.svg" />
+      <img class="logo" src="/images/logo-footer.svg" alt="Breaking Barriers Building Bridges" />
       <p class="footer__tagline">A Southern-led research network to advance gender equality in STEM</p>
       <chapterList /> 
       <p class="footer__text">The projects have worked to identify context-specific barriers and pilot both known and innovative interventions. These have included pedagogical practices, policies, institutionalized data gathering, and gender-responsive methodologies applied in some higher education institutions and STEM fields. Project leaders have seen success, with initiatives such as co-developing sexual harassment and gender equity policies alongside national ministries and replicating efforts across local institutions. For instance, in Zimbabwe, a project team is assisting the Ministry of Higher Education and Science and Technology in setting an agenda and developing policies on sexual harassment and addressing broader gender equity issues. However, there is a need to explore how these interventions can be scaled up by involving policymakers and other key STI ecosystem stakeholders from the start.</p>
